afbf72f4de465788b8bc7242d7959280d62b5d80
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2017-02-12 21:54:30 -0600

    changed the home page to the index.html. Also commented out unessary codes that might be in use in the future -DT

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,14 +1,14 @@
 //requiring files
 const express = require("express");
 const app = express();
-const db = require("../models");
+const path = require("path");
+
+// const db = require("../models");
 
 //creating different routes for special events. along with that we are using the models directory (sequelize)
 module.exports = function(app){
   app.get("/", function(req, res) {
-    db.users.findAll().then(function(result){
-      res.send(result);
-    });
+    res.sendFile(path.join(__dirname + "/../public", "index.html"));
   });
 
 	//more stuff will be added
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 //required npm to get this app to work
 const express = require("express");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const path = require("path");
-const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
+
+const bodyParser = require("body-parser");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -17,8 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// const methodOverride = require("method-override");
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 //getting handlebars so we can have a webpage
 // var exphbs = require("express-handlebars");
@@ -29,7 +28,9 @@ app.use(methodOverride("_method"));
 //requiring and calling the function that was exported from the controllers file. It will get the webpage and post it in the local host. At the same time it will listen for certain request so i can edit the DB and then refresh the page
 require("./controllers/users_controllers.js")(app);
 
+// const db = require("./models");
+
 //after connecting to the DB base with sequelize, it will create a localhost so the user can view the page
-db.sequelize.sync().then(function(){
-	app.listen(process.env.PORT || 5000);
-});
+// db.sequelize.sync().then(function(){
+	app.listen(process.env.PORT || 8080);
+// });
