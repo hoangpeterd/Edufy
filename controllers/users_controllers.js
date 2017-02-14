e9a02560829085ace27f887fb39e984ca21179ee
yashaRO <yashanyou@gmail.com>
2017-02-13 18:38:10 -0600

    Trying to Pull

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -2,15 +2,33 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const db = require("../models");
 
 //creating different routes for special events. along with that we are using the models directory (sequelize)
 module.exports = function(app){
+  
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public", "index.html"));
   });
-
+  
+  //Page for testing out file sending
+  app.get('/upload', function(req, res) {
+    res.sendFile(path.join(__dirname +'/../public', 'SampleUpload.html'))
+  })
+  app.post('/upload1', function(req, res) {
+    
+    upload = req.files.transcript;
+    console.log(upload.name)
+    if (!req.files) {
+      res.send('No files were uploaded');
+      return;
+    }
+    upload.mv(path.join(__dirname + '/../private/uploadFiles/' + upload.name), function (err) {
+      if (err) {res.status(500).send(err)}
+      else {res.send('File uploaded!')}
+    })
+  })
+  //end test
   app.post("/signing", function(req, res) {
     db.users.count({ where: { username: req.body.userName } })
       .then(count => {
