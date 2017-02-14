fb4e63349dee5b6fdd5ee0b847db1854f150334c
yashaRO <yashanyou@gmail.com>
2017-02-13 18:40:18 -0600

    node_modules removed from git

---
+++ key.js
@@ -0,0 +1,5 @@
+var key = {
+    mysql: ""
+};
+
+module.exports = key;
--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/public"));
 
 const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload')
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(fileUpload());
 
 // const methodOverride = require("method-override");
 // Override with POST having ?_method=DELETE
