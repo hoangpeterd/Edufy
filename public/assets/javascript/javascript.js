921e943f14f415de2eb753188a65f29b29d58bc8
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2017-02-14 10:12:18 -0600

    created a sign up page with validations. Gonna get it to post in database soon -DT

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,14 +1,52 @@
 //checking if this is an email or not
-function validateForm(email) {
+function validateEmail(email) {
     var x = email;
     var atPos = x.indexOf("@");
     var dotPos = x.lastIndexOf(".");
 
-    if (atPos<1 || dotPos<atPos+2 || dotPos+2>=x.length || !x.endsWith("edu")) {
-      console.log("Not a valid e-mail address");
+    if (atPos<1 || dotPos<atPos+2 || dotPos+2>=x.length || !x.endsWith(".edu")) {
+      console.log("Not a valid e-mail address"); //setting up a warning
     } else {
-			console.log("True");
+			return true;
 		}
+}
+
+function validatePassword(password){
+  if(password.length<6){
+    console.log("Password to short");
+  } else {
+    return true;
+  }
+}
+
+function creatingUser (){
+  var email = "";
+  var password = "";
+  var position = "";
+
+  if(validateEmail($("#createUser").val())){
+    email = $("#createUser").val();
+  }
+
+  if(validatePassword($("#createPassword").val())){
+    password = $("#createPassword").val();
+  }
+
+  position = $("#creatPosition").val();
+
+  if(email !== "" && password!== "" && position !== ""){
+    var createObject = {
+      email: email,
+      password: password,
+      position: position
+    }
+
+    $.post("/signup", createObject).done(function(result) {
+
+    });
+  }
+
+
 }
 
 function signingIn (info){
@@ -27,5 +65,9 @@ $("document").ready(function(){
     };
 
     signingIn(info);
+  });
+
+  $("#createBtn").on("click", function(){
+    creatingUser();
   });
 });
