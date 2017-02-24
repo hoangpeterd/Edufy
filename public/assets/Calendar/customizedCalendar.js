595769e872656cde1e3c272ead07043885411afb
rxtATX <rachel.thiim@gmail.com>
2017-02-23 20:09:00 -0600

    Made a handful of changes to calendar including blocking creating events on month view, attempting to constrain selectable times to business hours, and started student calendar view.

--- public/assets/Calendar/customizedCalendar.js
+++ public/assets/Calendar/customizedCalendar.js
@@ -1,16 +1,17 @@
 var i = 0;
+console.log(moment().format());
 var newEvent = {
 	id: i, 
 	start: null, 
 	end: null, 
-	title: null
-	// , allDay: false
+	title: null,
 };
 var FC = $.fullCalendar;
 
 $(function() { // document ready
 	/* initialize the external events
 	-----------------------------------------------------------------*/
+
 	$('#external-events .fc-event').each(function() {
 
 		// store data so the calendar knows to render an event upon drop
@@ -18,14 +19,12 @@ $(function() { // document ready
 			stick: true // maintain when user navigates (see docs on the renderEvent method)
 		});
 
-		$("#calendar").fullCalendar("addEventSource", userSpecTime);
 
 		// make the event draggable using jQuery UI
 		$(this).draggable({
 			zIndex: 999,
 			revert: true,      // will cause the event to go back to its
 			revertDuration: 0  //  original position after the drag
-			
 		});
 	});
 
@@ -42,23 +41,34 @@ $(function() { // document ready
 		eventOverlap: false,
 		slotDuration: '00:60:00',
 
-		//This element should be custom based on User's registration.
-		// businessHours: userSpecTime,
-
-		displayEventTime: true,
-		displayEventEnd: true,
-		scrollTime: '00:00', // undo default 6am scrollTime
+		slotLabelFormat: 'h(:mm)a',
+		businessHours: FC.businessHours.businessHours,	
+		// displayEventTime: true,
+		// displayEventEnd: true,
 		header: {
 			left: 'today prev,next',
 			center: 'title',
 			right: 'agendaWeek,month,listMonth'
 		},
 		defaultView: 'month',
+		views: {
+			month: {
+				selectable: false
+			}
+		},
 		events: FC.events,
 		eventBorderColor: "#4CAE4C", 
 		eventBackgroundColor: "rgba(76, 174, 76, .5)",
+		// eventClick: function(data, event, view) {
+		// 	var content = $("<input type='text' placeholder='Name this timeslot'></input>");
+
+		// 	tooltip.set({
+		// 		'content.text': content
+		// 	})
+		// 	.reposition(event).show(event);
+		// },
+        // dayClick: function() { tooltip.hide(); },
 		select: function(start, end, jsEvent, view) {
-			
 			newEvent.id = FC.events.events.length + 1;
 			newEvent.start = start.format();
 			newEvent.end = end.format();
@@ -68,24 +78,32 @@ $(function() { // document ready
 		}
 	});
 });
-
+function makeSelectable() {
+		FC.selectable = true;
+		FC.selectHelper = true;
+		FC.editable = true; // enable draggable events
+}
 	FC.events = {
 		events: [
-				{
-					"id": '1',
-					"start": '2017-02-06T10:00:00',
-					"end": '2017-02-06T16:00:00',
-					"title": 'Available Time'
-				}, {
-					"id": '2',
-					"start": '2017-02-14',
-					"end": '2017-02-14',
-					"title": "Valentine's Day"
-				}
-			],
+			{
+				"id": '1',
+				"start": '2017-02-06T10:00:00',
+				"end": '2017-02-06T16:00:00',
+				"title": 'Available Time',
+				"allDay": false
+			}, {
+				"id": '2',
+				"start": '2017-02-14',
+				"end": '2017-02-14',
+				"title": "Valentine's Day",
+				"allDay": true
+			}
+		],
 	};
 
-	userSpecTime = [ // specify an array instead
+	FC.businessHours = {  
+		//I want this to be customizeable based on Tutor's registration..
+		businessHours: [ // specify an array instead
     		{
 				dow: [ 1, 2, 3 ], // Monday, Tuesday, Wednesday
 				start: '16:30', // 
@@ -101,4 +119,31 @@ $(function() { // document ready
 				start: '14:00',
 				end: '18:00'
 			}
-		];
+		],
+	};
+
+	$("#sessions").fullCalendar({
+		schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+		header: {
+			left: 'today prev,next',
+			center: 'title',
+		},
+		defaultView: 'listMonth',
+		events: FC.events,
+		eventBorderColor: "#4CAE4C", 
+		eventBackgroundColor: "rgba(76, 174, 76, .5)",
+		dayClick: function(date, jsEvent, view) {
+			console.log(date.format());
+			console.log(jsEvent);
+			console.log(view.name);
+		},
+		eventConstraint: FC.businessHours,
+		select: function(start, end, jsEvent, view) {
+			newEvent.id = FC.events.events.length + 1;
+			newEvent.start = start.format();
+			newEvent.end = end.format();
+			newEvent.title = "Available Time"; 
+			FC.events.events.push(newEvent);
+			$("#calendar").fullCalendar("addEventSource", [newEvent]);
+		}
+	});
