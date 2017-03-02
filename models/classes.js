5f1db75ab857056b3bb72a53b6eec802300dbfcd
yashaRO <yashanyou@gmail.com>
2017-03-02 06:27:34 -0600

    I reallly like this one.

--- models/classes.js
+++ models/classes.js
@@ -1,45 +1,66 @@
 //Why query twice what you can query once. Give me all the tutors who have Science (AKA query for if not null)
 module.exports = function(sequelize, DataTypes){
+	
+	function isClassStyle(classList) {
+				
+		arr = classList.trim().replace(/\s+/g,'').toUpperCase().split(',')
+		for (let i = 0; i < arr.length; i++) {
+			if (!(/^[A-Z]{3,5}\d+$/.test(arr[i]))) {
+				throw new Error ('Incorrect Syntax. If not provided, follow this example: MATH2001, ...etc')
+			}
+		}
+	}
+	
   var Classes = sequelize.define("classes", {
     user_id: {
       type: DataTypes.INTEGER,
-      primaryKey: true
+      primaryKey: true,
+			isClassStyle: isClassStyle
     },
 		liberalArts: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     business: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     engineering: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     mathematics: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     biology: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     chemistry: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     compSci: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     geology: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     },
     physics: {
       type: DataTypes.STRING,
-			defaultValue: null
+			defaultValue: null,
+			isClassStyle: isClassStyle
     }
   }, {
     timestamps: false
