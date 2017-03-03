ea7297df39f8378b73e15195b315b2da135e669a
Peter Hoàng <peterhoang@utexas.edu>
2017-03-03 14:27:42 -0600

    Fixed the row for classes on the student page

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -7,34 +7,34 @@ function findRating (userName, cb){
     cb(rating);
   });
 }
-  
+
   $('.grid-item').on('click', function() {
-    
-    $('#accordion').empty()
+
+    // $('#accordion').empty()
     let subject = $(this).attr('value') 
     $.get('/class/' + subject).done(function(data) {
       console.log(data)
       $.each(data, function(index, value) {
-        
+
         let tutorName = "Becky" || "Becky"
         let classes = "MATH1202, DAVE3202, SHAN2342" || 2
         let link = 'assets/images/' + subject + ".gif" || 2
         let rating = 3 || 3
         let id = 'One'
-        
-        
-        var large = `<div class='panel panel-default'><div class='panel-heading' role='tab' id='heading${id}'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse${id}' aria-expanded='true' aria-controls='collapse${id}'><div class='row'><div class='col-xs-12' align='left'><div class='col-xs-6'><img class='img-rounded 'id='profileImage' width='55' height='55' src=${link}></div><div class='col-xs-6'><div class='row'>${tutorName}</div><div class='row'>${rating}</div><div class='row'>${classes}</div></div></div></div></a></h4></div><div id='collapse${id}' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading${id}'><div class='panel-body text-left'>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div></div></div>` 
-   
+
+
+        var large = `<div class='panel panel-default'><div class='panel-heading' role='tab' id='heading${id}'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#collapse${id}' aria-expanded='true' aria-controls='collapse${id}'><div class='row'><div class='col-xs-12' align='left'><div class='col-xs-6'><img class='img-rounded 'id='profileImage' width='55' height='55' src=${link}></div><div class='col-xs-6'><div class='row'>${tutorName}</div><div class='row'>${rating}</div><div class='row'>${classes}</div></div></div></div></a></h4></div><div id='collapse${id}' class='panel-collapse collapse' role='tabpanel' aria-labelledby='heading${id}'><div class='panel-body text-left'>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div></div></div>`
+
         $('#accordion').append(large)
       })
-          
-      
-      
-      
-      
+
+
+
+
+
     })
-  }) 
-  	
+  })
+
 
   //when clicked on the signin button a modal will show up
   $("#modalSignIn").on("click",function() {
@@ -101,7 +101,7 @@ function findRating (userName, cb){
 
 
 });
-  
+
   $('#lol').on('click', function() {
     jQuery.noConflict()
     $('#tutorClasses').modal()
@@ -110,7 +110,7 @@ function findRating (userName, cb){
     a = $(this).attr('value')
     console.log(a)
    // $.get('class/' + a).done(function(data) {
-      
+
   //  })
    // $().
   })
@@ -130,4 +130,3 @@ function findRating (userName, cb){
     });
   }
 });
-
